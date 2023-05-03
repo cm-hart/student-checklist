d82fd66095acc543b2bf323f3841623a262f688c
Catie Hart <cmh@anniecannons.com>
2023-05-03 14:50:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Amara
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Ann
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Cynthia
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Lavender
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Jasmine
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Jiggii
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Kia
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Melinda
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Shan
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <div className="label-input-wrapper">
+          <label for="student-name" name="student-check">
+            Taylor
+          </label>
+          <input className="check-box" type="checkbox" name="student-check" />
+        </div>
+        <button className="submit-button" type="submit">
+          Reset
+        </button>
+      </form>
     </div>
   );
 }
